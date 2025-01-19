4103a8eb9b323bf2e93c067cff7840286a710fd5
Victor <viar1889@gmail.com>
2025-01-19 13:05:12 -0500

    setting up render

--- ghost_app.py
+++ ghost_app.py
@@ -1,11 +1,18 @@
-from waitress import serve
-from flask import Flask, render_template
+import flask_cors
+import waitress
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/run-code', methods=['POST'])
+def run_code():
+    result = "Python code executed!"
+    return jsonify({'result': result})
+
+def start_server():
+    app = Flask(__name__)
+    flask_cors.CORS(app)
+    waitress.serve(app, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    start_server()
