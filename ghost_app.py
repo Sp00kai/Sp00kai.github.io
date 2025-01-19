08100fda4ef1ea2bfd1e7fbbe19f21e0ce32c3b5
Victor <viar1889@gmail.com>
2025-01-19 13:27:19 -0500

    test 1

--- ghost_app.py
+++ ghost_app.py
@@ -1,3 +1,4 @@
+import os
 import flask_cors
 import waitress
 from flask import Flask, jsonify, send_file
@@ -15,7 +16,9 @@ def run_code():
     return jsonify({'result': result})
 
 def start_server():
-    waitress.serve(app, host='0.0.0.0', port=8080)
+    # Get port from environment variable or default to 8080
+    port = int(os.environ.get('PORT', 4000))
+    waitress.serve(app, host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     start_server()
