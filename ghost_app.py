c606115b5be4cc8deaf2e33ae2b32e9b305946af
Victor <viar1889@gmail.com>
2025-01-19 13:14:25 -0500

    button attempt 3

--- ghost_app.py
+++ ghost_app.py
@@ -3,6 +3,11 @@ import waitress
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+flask_cors.CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/run-code', methods=['POST'])
 def run_code():
@@ -10,8 +15,6 @@ def run_code():
     return jsonify({'result': result})
 
 def start_server():
-    app = Flask(__name__)
-    flask_cors.CORS(app)
     waitress.serve(app, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
